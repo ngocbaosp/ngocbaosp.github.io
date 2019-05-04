75fc3025d5b316713266b679e8d31ccf07367709
Ngoc Bao <tnbao.dhsp@gmail.com>
2019-05-04 00:08:06 -0500

    Hello World Servlet

---
+++ HelloWorldServlet/src/mum/cs472/HelloWorldServlet.java
@@ -0,0 +1,33 @@
+package mum.cs472;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "HelloWorldServlet")
+public class HelloWorldServlet extends HttpServlet {
+
+    private String message;
+
+    public void init() throws ServletException {
+        message = "Hello World";
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        response.setContentType("text/html");
+
+        PrintWriter out = response.getWriter();
+        out.println("<h1>" + message + "</h1>");
+
+
+    }
+}
