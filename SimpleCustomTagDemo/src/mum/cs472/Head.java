67a59e1e2a4763bb9d3eb6a9e54d59f4f5efd9b9
Ngoc Bao <tnbao.dhsp@gmail.com>
2019-05-07 22:07:51 -0500

    Simple Custom Tag Demo

---
+++ SimpleCustomTagDemo/src/mum/cs472/Head.java
@@ -0,0 +1,28 @@
+package mum.cs472;
+
+import javax.servlet.jsp.*;
+import javax.servlet.jsp.tagext.*;
+import java.io.IOException;
+
+public class Head extends SimpleTagSupport {
+    String theColor;
+    String words;
+
+    //This is called from JSP servlet to render custom tag
+    public void doTag() throws JspException, IOException {
+        JspWriter out = getJspContext().getOut();
+        if (theColor != null)
+            out.write(String.format("<span style='color:%s'>%s</span>", theColor, words));
+        else
+            out.write(String.format("<span>%s</span>", words));
+    }
+
+    //Need a setter for each attribute of custom tag
+    public void setTheColor(String theColor) {
+        this.theColor = theColor;
+    }
+
+    public void setWords(String words) {
+        this.words = words;
+    }
+}
