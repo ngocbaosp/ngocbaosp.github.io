e7da38636bb583d6255fe4fa7e4e4fee4384c54c
Ngoc Bao <tnbao.dhsp@gmail.com>
2019-05-10 14:56:38 -0500

    Tip Calculate

--- JavaScriptLabs/js/tip.js
+++ JavaScriptLabs/js/tip.js
@@ -1,14 +1,32 @@
 function calcTip() {
     var subtotalElem = document.getElementById('subtotal');
-    var tipElem =document.getElementById('tip');
+    var tipElem = document.getElementById('tip');
     var totalElem = document.getElementById('total');
     var subtotal = parseFloat(subtotalElem.value);
     var tip = parseFloat(tipElem.value);
-    var total = subtotal+tip*subtotal/100;
+
+    if (validateNumber(subtotal)) {
+        if (validateNumber(tip) == false)
+            return;
+    } else
+        return;
+
+    var total = subtotal + tip * subtotal / 100;
+
     /*
     console.log(subtotalElem.value);
     console.log(tipElem.value);
     console.log(total);
     */
     totalElem.innerHTML = '$' + total;
+}
+
+function validateNumber(number) {
+
+    if (isNaN(number)) {
+        alert("Please input a number");
+        return false;
+    }
+    return true;
+
 }
