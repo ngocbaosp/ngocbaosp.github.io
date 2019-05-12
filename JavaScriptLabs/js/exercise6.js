855411bffb2fe6bc4c347a6b88afa65c185a0831
Ngoc Bao <tnbao.dhsp@gmail.com>
2019-05-11 21:40:41 -0500

    Javascript Exercises - Lesson 6

--- JavaScriptLabs/js/exercise6.js
+++ JavaScriptLabs/js/exercise6.js
@@ -1,17 +1,26 @@
 //setTimeout(booyah, 2000);
 //setTimeout(booyah(), 2000);
-setTimeout(booyah1, 2000);
-setTimeout(booyah2(), 10000);
+//setTimeout(booyah1, 2000);
+setTimeout(booyah2(), 2000);
 
+
+function getAlert(str) {
+    return function() { alert(str); }
+}
+var whatsUpAlert= getAlert("What's up!");
+whatsUpAlert(); // “What’s up!”
+getAlert("What's up up!")();
 function booyah() {
     alert("booyah");
 
 }
 
+//////////////////////////////////
 function booyah1() {
-    alert("BOOYAH!");
 
+    alert("BOOYAH!");
 }
+//////////////////////////////////
 function booyah2() {
-    setTimeout(booyah1,2000);
+    return booyah1;
 }
